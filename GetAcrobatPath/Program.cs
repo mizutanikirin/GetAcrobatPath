6e0bf20cc3b1f9977a43354dab7c71b5541af66a
mizutanikirin <mizutani@thedesignium.com>
2016-04-02 16:25:19 +0900

    init commit

---
+++ GetAcrobatPath/Program.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace PortableDevices
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            var adobe = Registry.LocalMachine.OpenSubKey("Software").OpenSubKey("Microsoft").OpenSubKey("Windows").OpenSubKey("CurrentVersion").OpenSubKey("App Paths").OpenSubKey("AcroRd32.exe");
+            var path = adobe.GetValue("");
+
+            Console.WriteLine(path);
+        }
+    }
+}
